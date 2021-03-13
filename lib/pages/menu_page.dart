02a9ac671e1e5011f5990c86b32115a8c08ee3c7
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-12 21:39:15 -0500

    Settings Page 5

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,15 +1,83 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_preferences/widgets/menu_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class MenuPage extends StatelessWidget {
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+
+  bool _color = false;
+  String _name = "";
+  String _address = "";
+  int _gender = 0;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getValues();
+  }
+
+  getValues() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _color = prefs.getBool("colorSecundario");
+    _name = prefs.getString("namex");
+    _address = prefs.getString("address");
+    _gender = prefs.getInt("genero");
+    setState(() {
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Shared Preferences"),
-        backgroundColor: Colors.teal,
+        backgroundColor: _color ? Colors.pinkAccent : Colors.teal,
       ),
-      drawer: MenuWidget()
+      drawer: MenuWidget(),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.greenAccent,
+            borderRadius: BorderRadius.circular(25.0)
+          ),
+          padding: EdgeInsets.all(25.0),
+          margin: EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(_name),
+                subtitle: Text("Nombres"),
+              ),
+              ListTile(
+                leading: Icon(Icons.location_on),
+                title: Text(_address),
+                subtitle: Text("Dirección"),
+              ),
+              ListTile(
+                leading: Icon(Icons.supervised_user_circle),
+                title: Text(_gender == 1 ? "Masculino" : "Femenino"),
+                subtitle: Text("Género"),
+              ),
+              ListTile(
+                leading: Icon(Icons.supervised_user_circle),
+                title: Text(_color ? "Activo" : "Desactivo"),
+                subtitle: Text("Color secundario"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
