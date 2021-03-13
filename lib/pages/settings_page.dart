1f5e8ab4ace9774926e8313a88d6c68e87be2e45
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-12 19:24:42 -0500

    Settings Page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,10 +1,91 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_preferences/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _colorSecundario = false;
+  int _genero = 1;
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    print("Crear widget");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Configuración"),
+        backgroundColor: Colors.teal,
+      ),
+      drawer: MenuWidget(),
+      body: Center(
+        child: ListView(
+          children: [
+            Text(
+              "Configuración",
+              style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Divider(),
+            SwitchListTile(
+              value: _colorSecundario,
+              title: Text("Color secundario"),
+              subtitle: Text("Si quieres puedes activarlo"),
+              onChanged: (valor) {
+                _colorSecundario = valor;
+                setState(() {});
+              },
+            ),
+            RadioListTile(
+              value: 1,
+              title: Text("Masculino"),
+              groupValue: _genero,
+              onChanged: (int valor){
+                setState(() {
+                  _genero = valor;
+                });
+              },
+            ),
+            RadioListTile(
+              value: 2,
+              title: Text("Femenino"),
+              groupValue: _genero,
+              onChanged: (int valor){
+                setState(() {
+                  _genero = valor;
+                });
+              },
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Nombres",
+                  helperText: "Ingresa tus nombres"
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Dirección",
+                    helperText: "Ingresa tu dirección"
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/menu_widget.dart
+++ lib/widgets/menu_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_preferences/pages/menu_page.dart';
 import 'package:flutter_codigo_preferences/pages/settings_page.dart';
 
 
@@ -31,7 +32,7 @@ class MenuWidget extends StatelessWidget {
             leading: Icon(Icons.pages, color: Colors.teal,),
             title: Text("Inicio"),
             onTap: (){
-
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>MenuPage()));
             },
           ),
           ListTile(
